0d3044fbf7d4466a5c5094ec4344e74ec522916b
EugeneGIS <eugeneliuhaokun@163.com>
2023-10-26 17:10:00 +0200

    Add initial speed and distance report

---
+++ R/Clusters.R
@@ -0,0 +1,11 @@
+#' Data of Hierarchical clustering result on SOM 
+#'
+#' Brief statement: This dataset should be used for the further mapping in R. 
+#'
+#' @format A tibble with 2145 rows and 20 variables, I just pick up the important columns:
+#' \describe{
+#'   \item{hc}{Results of hierarchical clustering was recorded. } 
+#'   \item{`subset2020$BFS_nummer`}{It should be used for the further merging with Swiss shapefile. no size}
+#' }
+#' @source \url{https://applicationspub.unil.ch/interpub/noauth/php/Un/UnPers.php?PerNum=1034786&LanCode=8}
+"Clusters"
